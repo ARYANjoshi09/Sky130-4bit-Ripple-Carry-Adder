d959c31e62e2e53022b4609544388ca7965c226b
Aryan Joshi <125539630+ARYANjoshi09@users.noreply.github.com>
2026-04-07 11:15:10 +0530

    Refactor testbench to use loop for input generation
    Replaced manual test cases with a loop for exhaustive testing of the 4-bit RCA.

--- tb/tb_rca_4bit.v
+++ tb/tb_rca_4bit.v
@@ -10,22 +10,17 @@ module tb_rca_4bit;
 
      rca_4bit uut (.A(A),.B(B),.cin(cin),.S(S),.cout(cout));
 
-     initial begin
-     $dumpfile("adderwaves.vcd");
-     $dumpvars(0, tb_rca_4bit);
-
-     //Test 1 : we have 4bit input so we can add min 0 to max 15;
-     // 10 + 3 = 13
-     A = 4'd10; B= 4'd3; cin = 0; #10;
-
-     //Test 2: 5 + 3
-     A = 4'd5; B = 4'd3; cin = 0; #10;
-
-     //Test 3: 
-     A = 4'd15; B = 4'd15; cin = 1; #10;
-
-     $display("Simulation Finished, Open GTKWave to see results");
-     $finish;
-
+integer i;
+initial begin
+    for (i = 0; i < 512; i = i + 1) begin
+        {cin, A, B} = i; #10;
+        if ({cout, S} !== (A + B + cin))
+            $display("FAIL: A=%0d B=%0d cin=%0d | Got=%0d Expected=%0d",
+                      A, B, cin, {cout,S}, A+B+cin);
     end
+    $display("All tests passed.");
+    $finish;
+end
+
+    
   endmodule
